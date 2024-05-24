96a0474337774d90752742d83f9739b4b6160f48
Steven Peralta <peralta.steven01@gmail.com>
2024-05-24 16:21:14 -0700

    Adjusted JS code to adjust name if input value is Eden or eden. Also fixed div container in plan.html to only include the name and moved the name to the top center of the screen using CSS.

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-
-// export function generateTitle() {
-//   const btn = document.querySelector('button');
-//   btn.addEventListener('click', () => {
-//   const input = document.getElementById('#input-value').value;
-//   const container = document.querySelector('#container');
-//   const title = document.createElement('h1');
-//   title.classList.add('head');
-//   title.textContent = input;
-//   container.appendChild(title);
-//   });
-// }
-
 const container = document.querySelector('.container');
 
-const firstName = document.getElementById('input-value');
+let firstName = document.getElementById('input-value');
 
 const btn = document.querySelector('button');
 
@@ -23,12 +10,25 @@ function makeTitle() {
   btn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const userName = firstName.value;
+    let total = '';
+
+    let userName = firstName.value + '\'s Meal Plan';
+    if (firstName.value === 'Eden') {
+      let total = 'Edensito\'s Meal Plan';
+      userName = total;
+    } else if (firstName.value === 'eden') {
+      let total = 'Edensito\'s Meal Plan';
+      userName = total;
+    }
   
     localStorage.setItem('first-name', userName);
 
     window.location.href = 'plan.html';
   });
+}
+
+function getWeight() {
+  
 }
 
 makeTitle();
